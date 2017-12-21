c2ad27c89f53ca352e5ee8bb165feca8f7ef6375
jessgriffin <jmgriffin@outlook.com>
2017-12-21 13:49:16 -0500

    Create LabSix.java

---
+++ LabSix.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class LabSix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		seriesCalculator thisSeries = new seriesCalculator();
+		
+		thisSeries.getValues();
+		thisSeries.calculateSeries();
+		thisSeries.display();
+	}
+
+}
